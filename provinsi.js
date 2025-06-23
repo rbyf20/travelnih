69ba8ec8a977889275940c986cc223e54f48e483
rbyf20 <rabbyfajar34@gmail.com>
2025-06-23 21:40:29 +0700

    Add files via upload

---
+++ provinsi.js
@@ -0,0 +1,18 @@
+
+const DAFTAR_PROVINSI = [
+    "DKI Jakarta",
+    "Jawa Barat",
+    "Jawa Tengah",
+    "Jawa Timur",
+    "Banten",
+    "Bali",
+    "Sumatera Utara",
+    "Sumatera Barat",
+    "Sumatera Selatan",
+    "Lampung",
+    "Kalimantan Barat",
+    "Kalimantan Timur",
+    "Sulawesi Selatan",
+    "Sulawesi Utara",
+    "Papua"
+];
